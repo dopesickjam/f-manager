6c4e6484795ffcf227a4924d4b6f2f29fc089bb6
dopesickjam <dopesickjam@gmail.com>
2023-12-10 10:40:18 +0200

    add sort by transaction type

--- pages/transactions.py
+++ pages/transactions.py
@@ -6,14 +6,19 @@ from shared.db import create_sqlite_connection, fetch_data
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 # TO DO
-# add opportunity to select only desired Type transaction: ALL, Incomes, Expenses, Transfer
 # add filter by date FROM TO
 # to sum all incomes and expenses transaction by filter
 # add statistic by category
 # add opportunity to delete a transaction
 def main():
-    data = fetch_data("SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions")
+    option = st.selectbox(
+    'Chose transaction list',
+    ('ALL', 'Expense', 'Income', 'Transfer'))
 
+    if option == 'ALL':
+        data = fetch_data("SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions")
+    else:
+        data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions WHERE transaction_type='{option}'")
     #
     columns = ["Type", "Account", "Category", "Amount", "Currency", "Date", "Comment"]
     df = pd.DataFrame(data, columns=columns)
