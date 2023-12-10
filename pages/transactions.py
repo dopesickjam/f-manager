8e91a9eb02cf0c48612b728b2528169cdc946640
dopesickjam <dopesickjam@gmail.com>
2023-12-10 15:30:32 +0200

    completed from to do: filter by date FROM TO, summ all incomes or expenses

--- pages/transactions.py
+++ pages/transactions.py
@@ -6,16 +6,12 @@ from shared.db import create_sqlite_connection, fetch_data
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 # TO DO
-# add filter by date FROM TO
-# to sum all incomes and expenses transaction by filter
 # add statistic by category
 # add opportunity to delete a transaction
 def main():
     option = st.selectbox(
     'Chose transaction list',
     ('ALL', 'Expense', 'Income', 'Transfer'))
-    to_date = datetime.today()
-    from_date   = datetime.today() - timedelta(days=365)
     on = st.toggle('More filters:')
     if on:
         col1, col2 = st.columns(2)
@@ -25,14 +21,26 @@ def main():
         with col2:
             to_date = st.date_input("TO:", value="today", format="YYYY-MM-DD")
 
-    if option == 'ALL':
-        data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions WHERE transaction_date BETWEEN '{from_date}' AND '{to_date}'")
+    if on:
+        if option == 'ALL':
+            data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions WHERE transaction_date BETWEEN '{from_date}' AND '{to_date}'")
+        else:
+            data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions WHERE transaction_type='{option}' AND transaction_date BETWEEN '{from_date}' AND '{to_date}'")
     else:
-        data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions WHERE transaction_type='{option}' AND transaction_date BETWEEN '{from_date}' AND '{to_date}'")
+        if option == 'ALL':
+            data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions")
+        else:
+            data = fetch_data(f"SELECT transaction_type, account, category, amount, currency, transaction_date, transaction_description FROM transactions WHERE transaction_type='{option}'")
+
     #
     columns = ["Type", "Account", "Category", "Amount", "Currency", "Date", "Comment"]
     df = pd.DataFrame(data, columns=columns)
     st.dataframe(df)
+    summ = 0
+    if option == 'Expense' or option == 'Income':
+        for transaction in data:
+            summ = summ + float(transaction[3])
+        st.text(f'Summ of {option}s: {summ}')
 
 if __name__ == "__main__":
     main()
